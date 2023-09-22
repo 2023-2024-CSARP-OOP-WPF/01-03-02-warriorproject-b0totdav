e6a5a31d3e2875c50faa6d698740529cd2996024
b0totdav@vasvari.org <b0totdav@vasvari.org>
2023-09-22 14:25:15 +0200

    a

--- WarriorProject/Program.cs
+++ WarriorProject/Program.cs
@@ -12,8 +12,8 @@ namespace WarriorProject
         private int _x;
         private int _y;
 
-        public int X { get => _x; }
-        public int Y { get => _y; set => _y; }
+        public int X { get => _x; set => value; }
+        public int Y { get => _y; set => value; }
 
         public Position(int x, int y)
         {
