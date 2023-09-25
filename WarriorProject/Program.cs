00f20e9665f36474c99820bcac21c78e67ebd47a
b0totdav@vasvari.org <b0totdav@vasvari.org>
2023-09-25 08:32:05 +0200

    működő program

--- WarriorProject/Program.cs
+++ WarriorProject/Program.cs
@@ -12,8 +12,8 @@ namespace WarriorProject
         private int _x;
         private int _y;
 
-        public int X { get => _x; set => value; }
-        public int Y { get => _y; set => value; }
+        public int X { get => _x; set { _x = value; }}
+        public int Y { get => _y; set { _y = value; }}
 
         public Position(int x, int y)
         {
@@ -22,7 +22,7 @@ namespace WarriorProject
         }
         public void ToConsole()
         {
-            Console.Write($"({_x};{_y})");
+            Console.WriteLine($"({_x},{_y})");
         }
        
     }
@@ -38,18 +38,29 @@ namespace WarriorProject
         }
         public void ToConsole()
         {
-            Console.WriteLine($"{_name}, ({_position.X};{_position.Y})");
+            Console.WriteLine($"{_name}, position ({_position.X},{_position.Y})");
         }
         public void MoveHorizontal(int distance)
         {
             _position.X += distance;
+        }
+
+        public void MoveVertical(int distance)
+        {
+            _position.Y += distance;
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position position = new Position(2, -1);
+            position.ToConsole();
+            Warrior warrior = new Warrior("Peter the Iron Man", position);
+            warrior.ToConsole();
+            warrior.MoveHorizontal(3);
+            warrior.MoveVertical(-2);
+            warrior.ToConsole();
         }
     }
 }
